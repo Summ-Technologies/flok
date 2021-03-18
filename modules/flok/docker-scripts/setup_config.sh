41ef0ac6559b3ce7fc363f978246f0d66af1833e
Jared Hanson <jred0011@gmail.com>
2021-03-18 01:31:42 -0700

    Added docker scripts

---
+++ modules/flok/docker-scripts/setup_config.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+if [[ -z "$API_URL" ]]; then
+    echo "API_URL environment variable needs to be present."
+    exit 1
+fi
+if [[ -z "$IMAGES_URL" ]]; then
+    echo "IMAGES_URL environment variable needs to be present."
+    exit
+fi
+echo "window.appConfig={}" > /usr/share/nginx/html/config.js
+echo "window.appConfig.server_base_url=\"$API_URL\"" >> /usr/share/nginx/html/config.js
+echo "window.appConfig.images_base_url=\"$IMAGES_URL\"" >> /usr/share/nginx/html/config.js
