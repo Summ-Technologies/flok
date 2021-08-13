d544c8dc326a74f9d406faab26b4716bd448584f
Jared Hanson <jred0011@gmail.com>
2021-08-13 04:34:40 -0700

    Production mixpanel failing because setting empty token

--- modules/flok/docker-scripts/setup_config.sh
+++ modules/flok/docker-scripts/setup_config.sh
@@ -7,10 +7,14 @@ if [[ -z "$IMAGES_URL" ]]; then
     echo "IMAGES_URL environment variable needs to be present."
     exit 1
 fi
-if [[ -z "$MIXPANEL_TOKEN" ]]; then
-    echo "STRIPE_KEY environment variable not set. Payment's will fail."
-fi
+
 echo "window.appConfig={}" > /usr/share/nginx/html/config.js
 echo "window.appConfig.server_base_url=\"$API_URL\"" >> /usr/share/nginx/html/config.js
 echo "window.appConfig.images_base_url=\"$IMAGES_URL\"" >> /usr/share/nginx/html/config.js
-echo "window.appConfig.mixpanel_token=\"$MIXPANEL_TOKEN\"" >> /usr/share/nginx/html/config.js
+
+if [[ -z "$MIXPANEL_TOKEN" ]]; then
+    echo "MIXPANEL_TOKEN environment variable not set. Analytics not recording."
+else
+    echo "window.appConfig.mixpanel_token=\"$MIXPANEL_TOKEN\"" >> /usr/share/nginx/html/config.js
+fi
+
