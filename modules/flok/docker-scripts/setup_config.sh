be9e200b867fd9809123d87569bf52265e57a9ce
Jared Hanson <jred0011@gmail.com>
2021-09-25 00:27:46 -0700

    Updated setup config.js again

--- modules/flok/docker-scripts/setup_config.sh
+++ modules/flok/docker-scripts/setup_config.sh
@@ -19,33 +19,14 @@ else
 fi
 
 ####### Google & google maps config #######
-if [[ -z "$GOOGLE_API_KEY" ]]; then
-else
-    echo "window.appConfig.google_api_key=\"$GOOGLE_API_KEY\"" >> /usr/share/nginx/html/config.js
-fi
-
-if [[ -z "$GOOGLE_MAPS_ID_HOTEL_PAGE" ]]; then
-else
-    echo "window.appConfig.google_maps_id_hotel_page=\"$GOOGLE_MAPS_ID_HOTEL_PAGE\"" >> /usr/share/nginx/html/config.js
-fi
+[[ ! -z "$GOOGLE_MAPS_ID_HOTEL_PAGE" ]] && echo "window.appConfig.google_maps_id_hotel_page=\"$GOOGLE_MAPS_ID_HOTEL_PAGE\"" >> /usr/share/nginx/html/config.js
+[[ ! -z "$GOOGLE_API_KEY" ]] && echo "window.appConfig.google_api_key=\"$GOOGLE_API_KEY\"" >> /usr/share/nginx/html/config.js
 
 ####### Algolia config #######
-if [[ -z "$ALGOLIA_API_KEY" ]]; then
-else
-    echo "window.appConfig.algolia_api_key=\"$ALGOLIA_API_KEY\"" >> /usr/share/nginx/html/config.js
-fi
-if [[ -z "$ALGOLIA_APP_ID" ]]; then
-else
-    echo "window.appConfig.algolia_app_id=\"$ALGOLIA_APP_ID\"" >> /usr/share/nginx/html/config.js
-fi
-if [[ -z "$ALGOLIA_DESTINATIONS_INDEX" ]]; then
-else
-    echo "window.appConfig.algolia_destinations_index=\"$ALGOLIA_DESTINATIONS_INDEX\"" >> /usr/share/nginx/html/config.js
-fi
-if [[ -z "$ALGOLIA_HOTELS_INDEX" ]]; then
-else
-    echo "window.appConfig.algolia_hotels_index=\"$ALGOLIA_HOTELS_INDEX\"" >> /usr/share/nginx/html/config.js
-fi
+[[ ! -z "$ALGOLIA_API_KEY" ]] && echo "window.appConfig.algolia_api_key=\"$ALGOLIA_API_KEY\"" >> /usr/share/nginx/html/config.js
+[[ ! -z "$ALGOLIA_APP_ID" ]] && echo "window.appConfig.algolia_app_id=\"$ALGOLIA_APP_ID\"" >> /usr/share/nginx/html/config.js
+[[ ! -z "$ALGOLIA_DESTINATIONS_INDEX" ]] && echo "window.appConfig.algolia_destinations_index=\"$ALGOLIA_DESTINATIONS_INDEX\"" >> /usr/share/nginx/html/config.js
+[[ ! -z "$ALGOLIA_HOTELS_INDEX" ]] && echo "window.appConfig.algolia_hotels_index=\"$ALGOLIA_HOTELS_INDEX\"" >> /usr/share/nginx/html/config.js
 
 echo "Final config.js file"
 cat /usr/share/nginx/html/config.js
