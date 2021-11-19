caea1a0babafb76fac6777a467958487fcc8f810
jacobhanson <jhanreg11@gmail.com>
2021-11-18 16:40:51 -0800

    add chips and location name to hotel details page

--- modules/flok/src/pages/HotelPage.tsx
+++ modules/flok/src/pages/HotelPage.tsx
@@ -1,4 +1,4 @@
-import {Button, Hidden, makeStyles} from "@material-ui/core"
+import {Button, Chip, Hidden, makeStyles} from "@material-ui/core"
 import {useDispatch, useSelector} from "react-redux"
 import {RouteComponentProps, withRouter} from "react-router-dom"
 import AppImageGrid from "../components/base/AppImageGrid"
@@ -19,7 +19,12 @@ import {
   postSelectedRetreatHotel,
 } from "../store/actions/retreat"
 import {convertGuid} from "../utils"
-import {useHotel} from "../utils/lodgingUtils"
+import {
+  DestinationUtils,
+  HotelUtils,
+  useDestinations,
+  useHotel,
+} from "../utils/lodgingUtils"
 import NotFound404Page from "./misc/NotFound404Page"
 
 let useStyles = makeStyles((theme) => ({
@@ -33,6 +38,39 @@ let useStyles = makeStyles((theme) => ({
     paddingBottom: theme.spacing(1),
     "& > *:not(:first-child)": {
       marginTop: theme.spacing(1),
+    },
+  },
+  attributesContainer: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginLeft: theme.spacing(-0.5),
+    marginBottom: theme.spacing(0.5),
+    "& > *": {
+      marginBottom: theme.spacing(0.5),
+      marginLeft: theme.spacing(0.5),
+    },
+  },
+  attributeTag: {
+    borderRadius: theme.shape.borderRadius,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    padding: theme.spacing(0.5),
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    backgroundColor: theme.palette.grey[300], // matching chip default background color
+  },
+  tagsContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+    marginLeft: theme.spacing(-0.5),
+    "& > *": {
+      marginLeft: theme.spacing(0.5),
+      marginBottom: theme.spacing(0.5),
     },
   },
 }))
@@ -52,6 +90,9 @@ function HotelPage(props: HotelPageProps) {
 
   // Get current hotel
   let hotel = useHotel(hotelGuid)
+
+  // Get all destination
+  let destinations = Object.values(useDestinations()[0])
 
   // Check if current hotel selected
   let selectedHotelIds = useSelector((state: RootState) => {
@@ -109,10 +150,64 @@ function HotelPage(props: HotelPageProps) {
               </PageOverlayFooterDefaultBody>
             }
             right={<AppImageGrid images={hotel.imgs} />}>
-            <PageHeader header={hotel.name} subheader={hotel.tagline} />
+            <PageHeader
+              header={hotel.name}
+              subheader={
+                destinations.filter(
+                  (dest) =>
+                    dest.id ===
+                    (hotel !== undefined && hotel !== ResourceNotFound
+                      ? hotel.destination_id
+                      : -1000)
+                )[0]
+                  ? DestinationUtils.getLocationName(
+                      destinations.filter(
+                        (dest) =>
+                          dest.id ===
+                          (hotel !== undefined && hotel !== ResourceNotFound
+                            ? hotel.destination_id
+                            : -1000)
+                      )[0]
+                    )
+                  : ""
+              }
+            />
             <AppTypography variant="body1" paragraph>
               {hotel.description}
             </AppTypography>
+            <div className={classes.attributesContainer}>
+              <div className={classes.attributeTag}>
+                <AppTypography variant="body2" noWrap uppercase>
+                  Price
+                </AppTypography>
+                <AppTypography variant="body1" fontWeight="bold">
+                  {hotel.price}
+                </AppTypography>
+              </div>
+              {hotel.airport_travel_time ? (
+                <div className={classes.attributeTag}>
+                  <AppTypography variant="body2" noWrap uppercase>
+                    Airport Distance
+                  </AppTypography>
+                  <AppTypography variant="body1" fontWeight="bold">
+                    {HotelUtils.getAirportTravelTime(hotel.airport_travel_time)}
+                  </AppTypography>
+                </div>
+              ) : undefined}
+              <div className={classes.attributeTag}>
+                <AppTypography variant="body2" noWrap uppercase>
+                  Rooms
+                </AppTypography>
+                <AppTypography variant="body1" fontWeight="bold">
+                  {hotel.num_rooms}
+                </AppTypography>
+              </div>
+            </div>
+            <div className={classes.tagsContainer}>
+              {hotel.lodging_tags.map(({name}) => (
+                <Chip size="small" label={name} />
+              ))}
+            </div>
             {hotel.address_coordinates ? (
               <div className={classes.mapContainer}>
                 <AppHotelLocationMap
