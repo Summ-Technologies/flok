267cbe81ff9bb8a6a0479205d4ccfc116e3a7c04
Jared Hanson <jred0011@gmail.com>
2021-11-22 14:26:46 -0800

    Small changes to hotel proposals page

--- modules/flok/src/components/lodging/AppHotelCarousel.tsx
+++ modules/flok/src/components/lodging/AppHotelCarousel.tsx
@@ -6,27 +6,66 @@ import AppTypography from "../base/AppTypography"
 
 type AppHotelCarouselProps = {
   hotels: HotelModel[]
+  onViewHotel: (hotel: HotelModel) => void
 }
 
 const imgSize = 200
+const imgSizeMobile = 140
 let useStyles = makeStyles((theme) => ({
   root: {
     overflowX: "scroll",
     display: "flex",
     flexWrap: "nowrap",
     overflowY: "visible",
+    borderRadius: theme.shape.borderRadius,
   },
   card: {
-    borderRadius: 10,
+    cursor: "pointer",
+    "&:hover $img": {
+      filter: "blur(1px) brightness(75%)",
+    },
+    "&:hover $imgExplore": {
+      display: "block",
+    },
+    borderRadius: theme.shape.borderRadius,
     textAlign: "center",
-    margin: 12,
-    width: imgSize,
+    margin: theme.spacing(1),
+    padding: theme.spacing(1),
+    width: imgSize + theme.spacing(2),
+    [theme.breakpoints.down("sm")]: {
+      width: imgSizeMobile + theme.spacing(2),
+    },
+  },
+  imgContainer: {
+    position: "relative",
   },
   img: {
     width: imgSize,
     height: imgSize,
+    [theme.breakpoints.down("sm")]: {
+      width: imgSizeMobile,
+      height: imgSizeMobile,
+    },
     objectFit: "cover",
-    borderRadius: 4,
+    borderRadius: theme.shape.borderRadius,
+  },
+  imgExplore: {
+    display: "none",
+
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    borderRadius: "20px",
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: theme.palette.common.white,
+    color: theme.palette.common.white,
+
+    padding: theme.spacing(4),
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+    backgroundColor: "rgba(255,255,255,.2)",
   },
 }))
 
@@ -36,12 +75,17 @@ export function AppHotelCarousel(props: AppHotelCarouselProps) {
   return (
     <div className={classes.root}>
       {props.hotels.map((hotel) => (
-        <div className={classes.card}>
-          <img
-            className={classes.img}
-            src={hotel.spotlight_img.image_url}
-            alt={hotel.spotlight_img.alt}
-          />
+        <div className={classes.card} onClick={() => props.onViewHotel(hotel)}>
+          <div className={classes.imgContainer}>
+            <img
+              className={classes.img}
+              src={hotel.spotlight_img.image_url}
+              alt={hotel.spotlight_img.alt}
+            />
+            <AppTypography className={classes.imgExplore} variant="body1">
+              View
+            </AppTypography>
+          </div>
           <AppTypography variant="h4" noWrap>
             {hotel.name ? hotel.name : "\u00A0"}
           </AppTypography>
--- modules/flok/src/pages/HotelProposalWaitingPage.tsx
+++ modules/flok/src/pages/HotelProposalWaitingPage.tsx
@@ -11,6 +11,8 @@ import PageContainer from "../components/page/PageContainer"
 import PageHeader from "../components/page/PageHeader"
 import PageOverlay from "../components/page/PageOverlay"
 import {ResourceNotFound} from "../models"
+import {HotelModel} from "../models/lodging"
+import {AppRoutes} from "../Stack"
 import {RootState} from "../store"
 import {getHotels} from "../store/actions/lodging"
 import {convertGuid} from "../utils"
@@ -19,11 +21,23 @@ import {useRetreat} from "../utils/lodgingUtils"
 type HotelProposalWaitingPageProps = RouteComponentProps<{retreatGuid: string}>
 
 let useStyles = makeStyles((theme) => ({
+  body: {
+    "& > *:not(:first-child)": {
+      marginTop: theme.spacing(3),
+    },
+  },
+  section: {
+    "& > h3": {
+      marginBottom: theme.spacing(1),
+    },
+  },
+  contactSection: {},
   contact_img: {
     width: 150,
     height: 150,
     objectFit: "cover",
     marginRight: theme.spacing(1),
+    borderRadius: theme.shape.borderRadius,
   },
   contact_body: {
     display: "flex",
@@ -47,7 +61,19 @@ function HotelProposalWaitingPage(props: HotelProposalWaitingPageProps) {
         .substr(4)
       dispatch(getHotels(filters))
     }
-  }, [retreat])
+  }, [retreat, dispatch])
+
+  // Actions
+  function onViewHotel(hotel: HotelModel) {
+    const newTab = window.open(
+      AppRoutes.getPath("HotelPage", {
+        retreatGuid: props.match.params.retreatGuid,
+        hotelGuid: hotel.guid,
+      }),
+      "_blank"
+    )
+    newTab?.focus()
+  }
 
   return (
     <RetreatRequired retreatGuid={retreatGuid}>
@@ -67,61 +93,89 @@ function HotelProposalWaitingPage(props: HotelProposalWaitingPageProps) {
               <AppLodgingFlowTimeline currentStep="PROPOSAL" halfway />
             }
           />
-          <AppTypography variant="body1">
-            We will now negotiate with hotels on your behalf. Once we have an
-            update, we will contact you at{" "}
-            <a
-              href={`mailto:${
-                retreat && retreat !== ResourceNotFound && retreat.contact_email
-              }`}>
-              {retreat && retreat !== ResourceNotFound && retreat.contact_email}
-            </a>{" "}
-          </AppTypography>
-          <br />
-          <AppHotelCarousel
-            hotels={
-              retreat && retreat !== ResourceNotFound
-                ? retreat.selected_hotels_ids
-                    .map((id) => hotelsById[id])
-                    .filter((hotel) => hotel)
-                : []
-            }
-          />
-          <br />
-          <AppTypography variant="h4">
-            We're here with you the whole way
-          </AppTypography>{" "}
-          <div className={classes.contact_body}>
-            {retreat && retreat !== ResourceNotFound && (
-              <img
-                className={classes.contact_img}
-                src={
-                  "https://t4.ftcdn.net/jpg/02/19/63/31/360_F_219633151_BW6TD8D1EA9OqZu4JgdmeJGg4JBaiAHj.jpg"
+          <div className={classes.body}>
+            <div className={classes.section}>
+              <AppTypography variant="h3">What's next?</AppTypography>
+              <AppTypography variant="body1">
+                We will now negotiate with hotels on your behalf. Once we have
+                an update, we will contact you at{" "}
+                <a
+                  href={`mailto:${
+                    retreat &&
+                    retreat !== ResourceNotFound &&
+                    retreat.contact_email
+                  }`}>
+                  {retreat &&
+                    retreat !== ResourceNotFound &&
+                    retreat.contact_email}
+                </a>{" "}
+              </AppTypography>
+            </div>
+            <div className={classes.section}>
+              <AppTypography variant="h3">
+                Selected Hotels (
+                {
+                  (retreat && retreat !== ResourceNotFound
+                    ? retreat.selected_hotels_ids
+                        .map((id) => hotelsById[id])
+                        .filter((hotel) => hotel)
+                    : []
+                  ).length
                 }
-                alt="TODO"
+                )
+              </AppTypography>
+              <AppHotelCarousel
+                hotels={
+                  retreat && retreat !== ResourceNotFound
+                    ? retreat.selected_hotels_ids
+                        .map((id) => hotelsById[id])
+                        .filter((hotel) => hotel)
+                    : []
+                }
+                onViewHotel={onViewHotel}
               />
-            )}
-            <AppTypography variant="body1">
-              You've been assigned{" "}
-              <strong>
-                {retreat && retreat !== ResourceNotFound && "TODO"}
-              </strong>{" "}
-              as your bookng concierge! If you have any questions, you can email
-              him at{" "}
-              <a
-                href={`mailto:${
-                  retreat && retreat !== ResourceNotFound && "sam@goflok.com"
-                }`}>
-                {retreat && retreat !== ResourceNotFound && "TODO"}
-              </a>{" "}
-              or reach him during business hours (9am - 6pm) at{" "}
-              <a
-                href={`tel:${
-                  retreat && retreat !== ResourceNotFound && "(555)-123-4567"
-                }`}>
-                {retreat && retreat !== ResourceNotFound && "TODO"}
-              </a>
-            </AppTypography>
+            </div>
+            <div className={classes.section}>
+              <AppTypography variant="h3">
+                We're with you the whole way
+              </AppTypography>{" "}
+              <div className={classes.contact_body}>
+                {retreat && retreat !== ResourceNotFound && (
+                  <img
+                    className={classes.contact_img}
+                    src={
+                      "https://t4.ftcdn.net/jpg/02/19/63/31/360_F_219633151_BW6TD8D1EA9OqZu4JgdmeJGg4JBaiAHj.jpg"
+                    }
+                    alt="TODO"
+                  />
+                )}
+                <AppTypography variant="body1">
+                  You've been assigned{" "}
+                  <strong>
+                    {retreat && retreat !== ResourceNotFound && "TODO"}
+                  </strong>{" "}
+                  as your bookng concierge! If you have any questions, you can
+                  email him at{" "}
+                  <a
+                    href={`mailto:${
+                      retreat &&
+                      retreat !== ResourceNotFound &&
+                      "sam@goflok.com"
+                    }`}>
+                    {retreat && retreat !== ResourceNotFound && "TODO"}
+                  </a>{" "}
+                  or reach him during business hours (9am - 6pm) at{" "}
+                  <a
+                    href={`tel:${
+                      retreat &&
+                      retreat !== ResourceNotFound &&
+                      "(555)-123-4567"
+                    }`}>
+                    {retreat && retreat !== ResourceNotFound && "TODO"}
+                  </a>
+                </AppTypography>
+              </div>
+            </div>
           </div>
         </PageOverlay>
       </PageContainer>
