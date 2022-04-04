04f3f2fe6e6a112a884f4c1c37c2f7293cdea6db
Andrew Busel <buselax@gmail.com>
2022-04-04 17:44:34 -0400

    changed dashboard release back to false

--- modules/flok/src/Stack.tsx
+++ modules/flok/src/Stack.tsx
@@ -69,7 +69,7 @@ export class AppRoutes {
 
 export default function Stack() {
   // TODO, remove once released dashboard
-  const DASHBOARD_RELEASE = true
+  const DASHBOARD_RELEASE = false
   if (!DASHBOARD_RELEASE) {
     return (
       <Switch>
