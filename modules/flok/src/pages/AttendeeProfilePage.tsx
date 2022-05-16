8205e5063b8ce19b27031c6feaa68842a1610061
Jared Hanson <jred0011@gmail.com>
2022-05-16 15:54:44 -0700

    Hotel check in fix and email editable

--- modules/flok/src/pages/AttendeeProfilePage.tsx
+++ modules/flok/src/pages/AttendeeProfilePage.tsx
@@ -291,13 +291,14 @@ function AttendeeProfilePage(props: AttendeesProfileProps) {
                         variant="outlined"
                         label="Email"
                         value={formik.values.email_address ?? ""}
+                        id="email_address"
                       />
                       <TextField
                         {...textFieldProps}
                         type="date"
                         className={classes.textField}
                         variant="outlined"
-                        label="Hotel check out"
+                        label="Hotel check in"
                         id="hotel_check_in"
                         value={formik.values.hotel_check_in ?? ""}
                       />
@@ -306,7 +307,7 @@ function AttendeeProfilePage(props: AttendeesProfileProps) {
                         type="date"
                         className={classes.textField}
                         variant="outlined"
-                        label="Hotel check in"
+                        label="Hotel check out"
                         id="hotel_check_out"
                         value={formik.values.hotel_check_out ?? ""}
                       />
