69d9851c42963303839f17c7312e6cb3187e140c
Andrew Busel <buselax@gmail.com>
2022-05-18 12:52:42 -0400

    removed inline styles, removed icon for mobile for copy button

--- modules/flok/src/components/base/AppShareableLinkModal.tsx
+++ modules/flok/src/components/base/AppShareableLinkModal.tsx
@@ -4,46 +4,69 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
+  makeStyles,
+  useMediaQuery,
 } from "@material-ui/core"
 import {Check, Link} from "@material-ui/icons"
 import {useState} from "react"
+import {FlokTheme} from "../../theme"
+
+let useStyles = makeStyles((theme) => ({
+  dialogWrapper: {
+    display: "flex",
+    alignItems: "center",
+    margin: 8,
+  },
+  linkText: {
+    backgroundColor: theme.palette.background.default,
+    padding: 3,
+    borderRadius: 4,
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    width: "90%",
+    maxWidth: 260,
+  },
+  copyButton: {
+    marginLeft: theme.spacing(0.25),
+  },
+}))
 type AppShareableLinkModalProps = {
   open: boolean
   link: string
   handleClose: () => void
 }
 function AppShareableLinkModal(props: AppShareableLinkModalProps) {
+  let classes = useStyles()
   let [copied, setCopied] = useState(false)
+  const isSmallScreen = useMediaQuery((theme: FlokTheme) =>
+    theme.breakpoints.down("sm")
+  )
+
   return (
     <Dialog open={props.open}>
       <DialogTitle>
         <Link fontSize="large" /> Get Shareable Link
       </DialogTitle>
       <DialogContent>
-        <div style={{display: "flex", alignItems: "center", margin: 8}}>
-          <div
-            style={{
-              backgroundColor: "#ECECEC",
-              padding: 3,
-              borderRadius: 8,
-              whiteSpace: "nowrap",
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-              width: "90%",
-              maxWidth: 260,
-            }}>
-            {props.link}
-          </div>
+        <div className={classes.dialogWrapper}>
+          <div className={classes.linkText}>{props.link}</div>
           <Button
             variant="outlined"
             color="primary"
             size="small"
-            style={{marginLeft: 2}}
+            className={classes.copyButton}
             onClick={() => {
               navigator.clipboard.writeText(props.link)
               setCopied(true)
             }}>
-            {copied ? (
+            {isSmallScreen ? (
+              copied ? (
+                "Copied"
+              ) : (
+                "Copy"
+              )
+            ) : copied ? (
               <>
                 <Check fontSize="small" /> Copied
               </>
