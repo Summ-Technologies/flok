65735c7fd23f1ade271510e0f936380ba05dc9a1
Andrew Busel <buselax@gmail.com>
2022-05-20 13:18:33 -0400

    added superscript denoting the difference in days

--- modules/flok/src/components/flights/FlightCard.tsx
+++ modules/flok/src/components/flights/FlightCard.tsx
@@ -110,7 +110,7 @@ let useStyles = makeStyles((theme) => ({
     width: "50%",
   },
   singleColumnContainer: {
-    width: "30%",
+    width: "35%",
   },
   columnInSingle: {
     width: "fit-content",
@@ -135,6 +135,11 @@ function FlightCard(props: FlightCardProps) {
   function chop(string: string) {
     //removes the last character from a string
     return string.substring(0, string.length - 1)
+  }
+  function differenceInDays(arr_datetime: Date, dep_datetime: Date) {
+    return Math.ceil(
+      (arr_datetime.getTime() - dep_datetime?.getTime()) / (1000 * 3600 * 24)
+    )
   }
 
   let dep_datetime = flight.dep_datetime
@@ -191,15 +196,29 @@ function FlightCard(props: FlightCardProps) {
       <div className={`${classes.twoColumns} ${classes.columnInDouble}`}>
         <div className={`${classes.column} ${classes.columnInDouble}`}>
           {dep_datetime && arr_datetime ? (
-            <Typography>
-              {new Intl.DateTimeFormat("en-GB", {
-                timeStyle: "short",
-              }).format(dep_datetime)}
-              {" - "}
-              {new Intl.DateTimeFormat("en-GB", {
-                timeStyle: "short",
-              }).format(arr_datetime)}
-            </Typography>
+            <>
+              <Typography>
+                {new Intl.DateTimeFormat("en-GB", {
+                  timeStyle: "short",
+                }).format(dep_datetime)}
+                {" - "}
+                {new Intl.DateTimeFormat("en-GB", {
+                  timeStyle: "short",
+                }).format(arr_datetime)}
+                {arr_datetime &&
+                  dep_datetime &&
+                  differenceInDays(arr_datetime, dep_datetime) > 0 && (
+                    <sup style={{marginLeft: "auto"}}>
+                      {"(+" +
+                        differenceInDays(
+                          arr_datetime,
+                          dep_datetime
+                        ).toString() +
+                        ")"}
+                    </sup>
+                  )}
+              </Typography>
+            </>
           ) : (
             "N/A"
           )}
